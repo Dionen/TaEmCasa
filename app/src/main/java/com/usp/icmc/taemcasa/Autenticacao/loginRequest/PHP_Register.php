c50cc1a97fbfd322e749559acff76290b231869a
João Guino <joao.rieswick@usp.br>
2017-06-02 15:32:35 -0300

    Retorna erro ao tentar cadastrar email repetido

--- app/src/main/java/com/usp/icmc/taemcasa/Autenticacao/loginRequest/PHP_Register.php
+++ app/src/main/java/com/usp/icmc/taemcasa/Autenticacao/loginRequest/PHP_Register.php
@@ -7,12 +7,23 @@
     $salt = $_POST["salt"];
     $hash = $_POST["hash"];
 
-    $statement = mysqli_prepare($con, "INSERT INTO users (nome, sobrenome, email, salt, hash) VALUES (?, ?, ?, ?, ?)");
-    mysqli_stmt_bind_param($statement, "sssss", $nome, $sobrenome, $email, $salt, $hash);
-    mysqli_stmt_execute($statement);
+    $statement2 = mysqli_prepare($con, "SELECT user_id FROM users WHERE email = ?");
+    mysqli_stmt_bind_param($statement2, "s", $email);
+    mysqli_stmt_execute($statement2);
+
+    mysqli_stmt_store_result($statement2);
 
     $response = array();
-    $response["success"] = true;
+    $response["success"] = false;
 
+    if(mysqli_stmt_fetch($statement2)){
+    } else {
+        $statement = mysqli_prepare($con, "INSERT INTO users (nome, sobrenome, email, salt, hash) VALUES (?, ?, ?, ?, ?)");
+        mysqli_stmt_bind_param($statement, "sssss", $nome, $sobrenome, $email, $salt, $hash);
+        mysqli_stmt_execute($statement);
+
+        $response["success"] = true;
+    }
+    
     echo json_encode($response);
 ?>
